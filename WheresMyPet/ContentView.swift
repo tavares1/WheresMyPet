1862e42f6b9563f2b631140043d9e7f590d90d3f
tavares <lucastavaresvasconcelos@gmail.com>
2019-10-19 10:42:34 -0300

    Initial Commit

---
+++ WheresMyPet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WheresMyPet
+//
+//  Created by Lucas Tavares on 19/10/19.
+//  Copyright © 2019 Lucas Tavares. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
